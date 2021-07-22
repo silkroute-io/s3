8c26f7bc32bc26402a70f50bcb4dc064efb5821e
Balbir Singh <balbir97@gmail.com>
2021-07-22 01:21:25 -0700

    first commit

---
+++ www.silkroute.io/assets/js/post_form_1.js
@@ -0,0 +1,22 @@
+jQuery(function($) {'use strict';
+              
+// Contact form
+var form = $('#form_1');
+form.submit(function(event){
+event.preventDefault();
+var form_status = $('<div class="form_status"></div>');
+
+$.ajax({
+url: $(this).attr('action'),
+data:form.serialize(),
+beforeSend: function(){
+form.prepend( form_status.html('<p style="text-align: center;"><i class="fa fa-spinner fa-spin"></i> Please wait for a moment...</p>').fadeIn() );
+}
+}).done(function(data){
+form_status.html('<p style="text-align: center;">Thank you for joining us !!</p>').delay(5000).fadeOut();
+});
+  
+$('#form_1')[0].reset();
+
+});
+});
---
+++ www.silkroute.io/assets/js/post_form_3.js
@@ -0,0 +1,22 @@
+jQuery(function($) {'use strict';
+              
+// Contact form
+var form = $('#form_3');
+form.submit(function(event){
+event.preventDefault();
+var form_status = $('<div class="form_status"></div>');
+
+$.ajax({
+url: $(this).attr('action'),
+data:form.serialize(),
+beforeSend: function(){
+form.prepend( form_status.html('<p style="text-align: center;"><i class="fa fa-spinner fa-spin"></i> Please wait for a moment...</p>').fadeIn() );
+}
+}).done(function(data){
+form_status.html('<p style="text-align: center;">Thank you for joining us !!</p>').delay(5000).fadeOut();
+});
+  
+$('#form_3')[0].reset();
+
+});
+});
